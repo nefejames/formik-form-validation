919187673fd91945685e1bfa34c20d1fdc0f1bed
nefejames <nefejames@yahoo.com>
2020-08-26 17:05:41 -0700

    formik && custom html elements

---
+++ src/SignInForm/SignInForm.jsx
@@ -0,0 +1,88 @@
+import React, { Fragment } from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+import "./SignInForm.scss";
+
+const signInSchema = Yup.object().shape({
+  email: Yup.string().email().required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password is too short - should be 6 chars minimum"),
+});
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const SignInForm = () => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={signInSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {(formik) => {
+        const {
+          values,
+          handleChange,
+          handleSubmit,
+          errors,
+          touched,
+          handleBlur,
+        } = formik;
+        return (
+          <Fragment>
+            <div className="container">
+              <h1>Sign in to continue</h1>
+              <form onSubmit={handleSubmit}>
+                <div className="form-row">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.email && touched.email ? "input-error" : null
+                    }
+                  />
+                  {errors.email && touched.email && (
+                    <span className="error">{errors.email}</span>
+                  )}
+                </div>
+
+                <div className="form-row">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={
+                      errors.password && touched.password ? "input-error" : null
+                    }
+                  />
+                  {errors.password && touched.password && (
+                    <span className="error">{errors.password}</span>
+                  )}
+                </div>
+
+                <button type="submit">Sign In</button>
+              </form>
+            </div>
+          </Fragment>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default SignInForm;
