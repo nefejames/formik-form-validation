e1d162c4ab11c2bbd20cfcd15288b28e7c4a5996
nefejames <nefejames@yahoo.com>
2020-08-30 14:31:56 -0700

    validation using Formik's custom components

--- src/SignInForm/SignInForm.jsx
+++ src/SignInForm/SignInForm.jsx
@@ -1,5 +1,5 @@
-import React, { Fragment } from "react";
-import { Formik } from "formik";
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 import "./SignInForm.scss";
@@ -26,59 +26,44 @@ const SignInForm = () => {
       }}
     >
       {(formik) => {
-        const {
-          values,
-          handleChange,
-          handleSubmit,
-          errors,
-          touched,
-          handleBlur,
-        } = formik;
+        const { errors, touched } = formik;
         return (
-          <Fragment>
-            <div className="container">
-              <h1>Sign in to continue</h1>
-              <form onSubmit={handleSubmit}>
-                <div className="form-row">
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={
-                      errors.email && touched.email ? "input-error" : null
-                    }
-                  />
-                  {errors.email && touched.email && (
-                    <span className="error">{errors.email}</span>
-                  )}
-                </div>
+          <div className="container">
+            <h1>Sign in to continue</h1>
+            <Form>
+              <div className="form-row">
+                <label htmlFor="email">Email</label>
+                <Field
+                  type="email"
+                  name="email"
+                  id="email"
+                  className={
+                    errors.email && touched.email ? "input-error" : null
+                  }
+                />
+                <ErrorMessage name="email" component="span" className="error" />
+              </div>
 
-                <div className="form-row">
-                  <label htmlFor="password">Password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    className={
-                      errors.password && touched.password ? "input-error" : null
-                    }
-                  />
-                  {errors.password && touched.password && (
-                    <span className="error">{errors.password}</span>
-                  )}
-                </div>
+              <div className="form-row">
+                <label htmlFor="password">Password</label>
+                <Field
+                  type="password"
+                  name="password"
+                  id="password"
+                  className={
+                    errors.password && touched.password ? "input-error" : null
+                  }
+                />
+                <ErrorMessage
+                  name="password"
+                  component="span"
+                  className="error"
+                />
+              </div>
 
-                <button type="submit">Sign In</button>
-              </form>
-            </div>
-          </Fragment>
+              <button type="submit">Sign In</button>
+            </Form>
+          </div>
         );
       }}
     </Formik>
